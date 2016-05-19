1a9acad1c8edb3963d96a788e20fdf09a72cc75d
Gonzalo Bulnes Guilpain <gonzalo.bulnes@redbubble.com>
2016-05-19 21:26:09 +1000

    Fix abusive error handling

--- lib/bathysphere/parser.rb
+++ lib/bathysphere/parser.rb
@@ -17,7 +17,7 @@ module Bathysphere
       keys(option_name).inject(raw_values(option_name)){ |data, key|
         begin
           data.fetch(refinements.shift.to_s)
-        rescue
+        rescue KeyError
           raise KeyError, "key not found #{option_name.inspect} in #{file}"
         end
       }
@@ -31,13 +31,13 @@ module Bathysphere
 
       def raw_values(option_name)
         @data.fetch(option_name.to_s).fetch('values', '')
-      rescue
+      rescue KeyError
         raise KeyError, "key not found #{option_name.inspect} in #{file}"
       end
 
       def raw_key(option_name)
         @data.fetch(option_name.to_s).fetch('key', '')
-      rescue
+      rescue KeyError
         raise KeyError, "key not found #{option_name.inspect} in #{file}"
       end
   end
