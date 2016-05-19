064726ed2089edc1c47219d5646f7180fa961c88
Gonzalo Bulnes Guilpain <gonzalo.bulnes@redbubble.com>
2016-05-19 17:53:56 +1000

    Partial add specs

--- spec/lib/bathysphere/parser_spec.rb
+++ spec/lib/bathysphere/parser_spec.rb
@@ -3,6 +3,29 @@ require 'spec_helper'
 module Bathysphere
   describe Parser do
 
-    it "exists"
+    let(:file) { 'spec/fixtures/fruit.yml' }
+    let(:parser) { Parser.new(file) }
+
+    it { expect(parser).to respond_to :fetch }
+
+    describe '#fetch' do
+
+      context 'when built from a valid YAML file' do
+
+        context 'when the property is available' do
+
+          it 'returns its value' do
+            expect(parser.fetch(:display_name, :large, :purple)).to eq 'Eggplant'
+          end
+        end
+
+        context 'when the propery is not available' do
+
+          it 'raises KeyError and mentions the parsed file in the message' do
+            expect{ parser.fetch(:weight, :large, :purple) }.to raise_error KeyError, Regexp.new(file)
+          end
+        end
+      end
+    end
   end
 end
