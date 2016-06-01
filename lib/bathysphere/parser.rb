bfad668cb73c83061084a1101f01f1cfa80d7759
Gonzalo Bulnes Guilpain <gonzalo.bulnes@redbubble.com>
2016-06-01 20:51:12 +1000

    Add Parser#using method

--- lib/bathysphere/parser.rb
+++ lib/bathysphere/parser.rb
@@ -3,12 +3,25 @@ require 'yaml'
 module Bathysphere
   class Parser
 
-    attr_reader :file
-    private :file
+    attr_reader :file, :refinements_store
+    private :file, :refinements_store
 
     def initialize(file_path)
       @file = file_path.downcase
       @data = YAML.load_file(file)
+    end
+
+    # Set a refinements store
+    #
+    # When a refinements store is available, the #fetch
+    # method will attempt to retrieve the refinements by
+    # calling reader methods on the store instead of using
+    # additional refinement arguments.
+    #
+    # Returns self (i.e. is a chainable method).
+    def using(refinements_store)
+      @refinements_store = refinements_store
+      self
     end
 
     def fetch(option_name, *refinements)
@@ -30,7 +43,10 @@ module Bathysphere
         keys(option_value).inject(values(option_value)) { |data, key|
           refinement = refinements.shift.to_s
           data.fetch(refinement) do
-            raise KeyError, "key not found #{refinement.inspect} in #{file}"
+            refinement = refinements_store.send(key).to_s
+            data.fetch(refinement) do
+              raise KeyError, "refinement not found #{refinement.inspect} in #{file}"
+            end
           end
         }
       end
